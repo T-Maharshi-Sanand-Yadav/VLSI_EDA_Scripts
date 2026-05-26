7f819a94f65ba54e3d964d84b736bb48f84940af
TMSY Tutorials <104819057+T-Maharshi-Sanand-Yadav@users.noreply.github.com>
2026-05-26 21:25:36 +0530

    Expand Genus pin attribute command notes

--- genus_commands/check_pin_clock_basic.tcl
+++ genus_commands/check_pin_clock_basic.tcl
@@ -18,3 +18,45 @@ get_db [get_pins -of_objects [get_cells {core/w_mem_inst_w_mem_reg[3][25]}] -fil
 # Meaning:
 #   The selected CK pin is associated with clock sha256/CLK.
 
+# First learn what attributes are available on this pin in your Genus version:
+get_db [get_pins -of_objects [get_cells {core/w_mem_inst_w_mem_reg[3][25]}] -filter "full_name=~*CK*"] .?
+
+# Common pin identity attributes:
+get_db [get_pins -of_objects [get_cells {core/w_mem_inst_w_mem_reg[3][25]}] -filter "full_name=~*CK*"] .name
+get_db [get_pins -of_objects [get_cells {core/w_mem_inst_w_mem_reg[3][25]}] -filter "full_name=~*CK*"] .full_name
+get_db [get_pins -of_objects [get_cells {core/w_mem_inst_w_mem_reg[3][25]}] -filter "full_name=~*CK*"] .base_name
+
+# Common pin type/connectivity attributes:
+get_db [get_pins -of_objects [get_cells {core/w_mem_inst_w_mem_reg[3][25]}] -filter "full_name=~*CK*"] .direction
+get_db [get_pins -of_objects [get_cells {core/w_mem_inst_w_mem_reg[3][25]}] -filter "full_name=~*CK*"] .net
+get_db [get_pins -of_objects [get_cells {core/w_mem_inst_w_mem_reg[3][25]}] -filter "full_name=~*CK*"] .clocks
+
+# If .net returns a net object, inspect that net:
+get_db [get_db [get_pins -of_objects [get_cells {core/w_mem_inst_w_mem_reg[3][25]}] -filter "full_name=~*CK*"] .net] .?
+get_db [get_db [get_pins -of_objects [get_cells {core/w_mem_inst_w_mem_reg[3][25]}] -filter "full_name=~*CK*"] .net] .name
+get_db [get_db [get_pins -of_objects [get_cells {core/w_mem_inst_w_mem_reg[3][25]}] -filter "full_name=~*CK*"] .net] .full_name
+get_db [get_db [get_pins -of_objects [get_cells {core/w_mem_inst_w_mem_reg[3][25]}] -filter "full_name=~*CK*"] .net] .drivers
+get_db [get_db [get_pins -of_objects [get_cells {core/w_mem_inst_w_mem_reg[3][25]}] -filter "full_name=~*CK*"] .net] .loads
+
+# Cleaner manual debug style using variables:
+set inst {core/w_mem_inst_w_mem_reg[3][25]}
+set cell [get_cells $inst]
+set pin  [get_pins -of_objects $cell -filter "full_name=~*CK*"]
+set net  [get_db $pin .net]
+
+get_db $pin .?
+get_db $pin .name
+get_db $pin .full_name
+get_db $pin .direction
+get_db $pin .clocks
+get_db $pin .net
+
+get_db $net .?
+get_db $net .name
+get_db $net .full_name
+get_db $net .drivers
+get_db $net .loads
+
+# Timing-based checks if you want to see the clock through timing reports:
+report_timing -to $pin
+report_timing -through $pin
